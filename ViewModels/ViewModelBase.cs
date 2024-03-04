98f9848594063eb5a142aa8ca18ae5ec21b1dcfc
JuanJoseTSP576 <jmahechav@uniempresarial.edu.co>
2024-03-03 21:02:40 -0500

    Funciones command
    Se han realizado las funciones para el login en el modelo

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace EmploymentManagement.ViewModels
+{
+    //Se pone abstracta para que solo se pueda usar mediante la herencia
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
+
